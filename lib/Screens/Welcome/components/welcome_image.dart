c71fa908a94ce7a440e260b4269abe55f22cfe5b
bintang.pkl <bintang.pkl@uii.ac.id>
2022-09-30 03:31:04 +0700

    login, regist, route

--- lib/Screens/Welcome/components/welcome_image.dart
+++ lib/Screens/Welcome/components/welcome_image.dart
@@ -12,10 +12,6 @@ class WelcomeImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          "WELCOME TO EDU",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
         SizedBox(height: defaultPadding * 2),
         Row(
           children: [
@@ -23,7 +19,7 @@ class WelcomeImage extends StatelessWidget {
             Expanded(
               flex: 8,
               child: SvgPicture.asset(
-                "assets/icons/chat.svg",
+                "assets/icons/tokoloak.svg",
               ),
             ),
             Spacer(),
